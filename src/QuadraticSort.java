b13ca409c1d1713909195f19fa1e8109fc6ffbed
Michael Wang <michaelwang1534@gmail.com>
2021-03-09 02:54:00 -0800

    Almost done, just debugging a few issues where the array sometimes comes back unsorted when running hybridsort

--- src/QuadraticSort.java
+++ src/QuadraticSort.java
@@ -1,3 +1,23 @@
 public class QuadraticSort {
+    //Will replicate inserstion sort because it is the fastest according to the chart and Part 1
+    //Requirement 2 & 3
+    private double[] arr;
+    private int index;
 
+    public QuadraticSort(double [] array) {
+        this.arr = array;
+        this.index = index;
+    }
+
+    public void quadraticSort (double[] arr, int left, int right) {
+        for (int i = left+1; i < right+1; i++) { //"i" is the 1st unsorted item
+            double temp = arr[i];
+            int j = i-1;
+            while (j >= left && arr[j] > temp) { //move (loop)
+                arr[j+1] = arr[j];
+                --j;
+            }
+            arr[j+1] = temp; //copy back
+        }
+    }
 }
--- src/RandomizedQuickSort.java
+++ src/RandomizedQuickSort.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class RandomizedQuickSort {
+    //Requirement 1
 
     private double[] arr;
 
@@ -11,20 +13,44 @@ public class RandomizedQuickSort {
 
     public int randomquicksort (double[] arr, int left, int right) {
         if (left < right) {
-            int index = partition(this.arr, left, right);
-//            randomquicksort(arr, left, index-1);
-//            randomquicksort(arr, index + 1, right);
+            int index = partition(arr, left, right);
+            randomquicksort(arr, left, index-1);
+            randomquicksort(arr, index + 1, right);
             return index;
         }
         return 0;
+    }
+
+    public void hybridsort (double[] arr, int left, int right) {
+        if (right - left > 1000) {
+            int partition = randomquicksort(arr, left, right);
+            if (partition - left < 1000) {
+                QuadraticSort quadraticSort = new QuadraticSort(arr);
+                quadraticSort.quadraticSort(arr, left, partition-1);
+            }
+            else {
+                hybridsort(arr, left, partition-1);
+            }
+            if (right - partition < 1000) {
+                QuadraticSort quadraticSort = new QuadraticSort(arr);
+                quadraticSort.quadraticSort(arr, partition+1, right);
+            }
+            else {
+                hybridsort(arr, partition+1, right);
+            }
+        }
+        else {
+            QuadraticSort quadraticSort = new QuadraticSort(arr);
+            quadraticSort.quadraticSort(arr, left, right);
+        }
     }
 
     int partition (double[] arr, int left, int right) {
         //Base case -- or reverse of the base case
         if (left < right) {
             Random generator = new Random();
-            int pivot = generator.nextInt(arr.length);
-            int bot = left + 1; //avoids re-sorting the pivot
+            int pivot = generator.nextInt(right-left) + left;
+            int bot = left; //avoids re-sorting the pivot
             int top = right;
             double store;
 
@@ -33,7 +59,7 @@ public class RandomizedQuickSort {
                 while (bot <= right && arr[bot] <= arr[pivot]) {
                     ++bot;
                 }
-                while (top >= bot && arr[top] > arr[pivot]) {
+                while (top >= bot && arr[top] >= arr[pivot]) {
                     --top;
                 }
                 if (bot <= right && bot < top) {
@@ -51,8 +77,38 @@ public class RandomizedQuickSort {
         return left;
     }
 
+    private static double[] randomDoubles(int size) {
+        double[] doublearray = new double[size];
+
+        for (int i = 0; i < size; i++) {
+            Random generator = new Random();
+            double randomDouble = generator.nextDouble();
+            doublearray[i] = randomDouble;
+        }
+        return doublearray;
+    }
+
+    public static boolean isSorted(double[] arr) { //for testing if the hybridsort works
+        for (int i = 0; i < arr.length-1; i++) {
+            if (arr[i] > arr[i+1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        RandomizedQuickSort randomizedQuickSort = new RandomizedQuickSort(new double[]{5, 2, 9, 12, 6, 8, 3, 7});
-        //randomizedQuickSort.randomquicksort();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter size of the double array (must be a multiple of 50,000!): ");;
+        int size = input.nextInt();
+        RandomizedQuickSort randomizedQuickSort = new RandomizedQuickSort(randomDoubles(size));
+        int index = randomizedQuickSort.randomquicksort(randomizedQuickSort.arr, 0,
+                randomizedQuickSort.arr.length-1);
+        System.out.println(isSorted(randomizedQuickSort.arr));
+        //System.out.println(index);
+
+//        randomizedQuickSort.hybridsort(randomizedQuickSort.arr, 0, randomizedQuickSort.arr.length-1);
+//        System.out.println(Arrays.toString(randomizedQuickSort.arr));
+//        System.out.println(isSorted(randomizedQuickSort.arr));
     }
 }
